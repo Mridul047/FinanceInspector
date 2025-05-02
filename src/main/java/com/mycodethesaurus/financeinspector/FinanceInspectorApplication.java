62334e7e9da23be3efc9ad9c2cde8bde383f654e
Mridul Choudhary <mridul.choudhary7@gmail.com>
2025-05-02 17:42:33 +0530

    Init commit

---
+++ src/main/java/com/mycodethesaurus/financeinspector/FinanceInspectorApplication.java
@@ -0,0 +1,13 @@
+package com.mycodethesaurus.financeinspector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceInspectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceInspectorApplication.class, args);
+	}
+
+}
