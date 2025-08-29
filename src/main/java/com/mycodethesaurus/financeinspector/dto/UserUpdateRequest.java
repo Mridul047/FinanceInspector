e18c3979abc25b21da0d3f5b7963e5ebb445a33d
Mridul Choudhary <mridul.choudhary7@gmail.com>
2025-08-29 20:16:57 +0530

    Add dto for req & res

---
+++ src/main/java/com/mycodethesaurus/financeinspector/dto/UserUpdateRequest.java
@@ -0,0 +1,22 @@
+package com.mycodethesaurus.financeinspector.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserUpdateRequest {
+
+  @Size(max = 100, message = "First name must not exceed 100 characters")
+  private String firstName;
+
+  @Size(max = 100, message = "Last name must not exceed 100 characters")
+  private String lastName;
+
+  @Email(message = "Email must be valid")
+  private String email;
+}
